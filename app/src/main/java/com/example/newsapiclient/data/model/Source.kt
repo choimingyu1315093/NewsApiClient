1599c0c42231a6c4467ecb83f3d5a6afd00a7dd5
choimingyu <alsrb1315093@gmail.com>
2024-11-28 10:14:10 +0900

    수정 완료

--- app/src/main/java/com/example/newsapiclient/data/model/Source.kt
+++ app/src/main/java/com/example/newsapiclient/data/model/Source.kt
@@ -1,10 +1,12 @@
 package com.example.newsapiclient.data.model
 
 import android.os.Parcelable
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Source(
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: String? = null,
     val name: String
 ): Parcelable
